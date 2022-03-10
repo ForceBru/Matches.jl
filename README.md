052a73aec711adca6141cde2d322aaadbaea3493
ForceBru <ForceBru@users.noreply.github.com>
2022-03-11 00:22:12 +0300

    feat: add README and example

---
+++ README.md
@@ -0,0 +1,43 @@
+# Matches.jl
+
+The most barebones machine learning "framework" that can compute gradients without backpropagation.
+Based on "Gradients without backpropagation" [1].
+
+The interface mimicks PyTorch, but since this package is just a toy, it's not a torch, but a bunch of matches :P
+
+## Basic usage
+
+```julia
+using Matches
+
+# 1. Get data.
+n_observations, n_features = 100, 5
+X, Y = randn(n_observations, n_features), randn(n_observations, 1)
+
+# 2. Build model, PyTorch-like.
+model = Sequential(
+    Linear(n_features, 7), Activations.sigmoid,
+    Linear(7, 5), Activations.tanh,
+    Linear(5, 1)
+)
+
+# 3. Set up gradient-descent optimizer.
+# This computes the "forward gradient" from the paper.
+optim = Descent(params(model), 1e-4)
+
+# 4. Train.
+for epoch in 1:50_000
+    random_dual!(optim)
+    loss = Losses.mse(model(X), Y)
+    step!(optim, loss)
+end
+
+# 5. Predict!
+Y_hat = model(X)
+```
+
+Also see <./example.jl>.
+
+## References
+
+1. Baydin, A. G., Pearlmutter, B. A., Syme, D., Wood, F., & Torr, P. (2022). Gradients without Backpropagation (Version 1). arXiv. <https://doi.org/10.48550/ARXIV.2202.08587>
---
+++ example.jl
@@ -0,0 +1,62 @@
+begin
+    @info "Setting up environment..."
+    import Pkg
+    Pkg.activate(temp=true)
+    Pkg.add(url="https://github.com/ForceBru/Matches.jl", io=devnull)
+    Pkg.add("UnicodePlots", io=devnull)
+    Pkg.status()
+end
+
+using Matches, UnicodePlots
+
+function to_windows(series::AbstractVector, win_size)
+    vcat([
+        series[off - win_size + 1:off]'
+        for off in win_size:length(series)
+    ]...)
+end
+
+# Data (n_observations, n_features)
+time_series = cumsum(randn(200))
+windows = to_windows(time_series, 20 + 1)
+
+X, Y = windows[:, 1:end-1], windows[:, end:end]
+@show size(X), size(Y)
+
+# Model
+model = Sequential(
+    Linear(20, 7), Activations.sigmoid,
+    Linear(7, 5), Activations.tanh,
+    Linear(5, 1)
+)
+
+# Optimizer
+optim = Descent(params(model), 1e-4)
+
+# Train
+epochs = 50_000
+loss_history = Real[]
+for epoch in 1:epochs
+    random_dual!(optim)
+    Y_hat = model(X)
+    loss = Losses.mse(Y_hat, Y)
+    step!(optim, loss)
+
+    push!(loss_history, real(loss))
+
+    if epoch % 500 == 0
+        @show epoch, real(loss)
+    end
+end
+
+# Plot the original time-series and the fit
+plt = let 
+    Y_original = Y[:, 1]
+    Y_fitted = model(TestMod.X)[:, 1] |> real
+    
+    plt = lineplot(Y_original, width=140, height=25)
+    lineplot!(plt, Y_fitted)
+    plt
+end
+
+display(plt)
