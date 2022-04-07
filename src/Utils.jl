667f22df143ad9f9900d5d48650b909431ec76b7
ForceBru <ForceBru@users.noreply.github.com>
2022-04-07 17:16:50 +0300

    bump version

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,4 +1,4 @@
 # Join tuples: https://discourse.julialang.org/t/efficient-tuple-concatenation/5398/9
-@inline tuplejoin(x::Tuple) = x
-@inline tuplejoin(x::Tuple, y::Tuple) = (x..., y...)
-@inline tuplejoin(x::Tuple, y::Tuple, z...) = (x..., tuplejoin(y, z...)...)
+@inline tuplejoin(x::Tuple)::Tuple = x
+@inline tuplejoin(x::Tuple, y::Tuple)::Tuple = (x..., y...)
+@inline tuplejoin(x::Tuple, y::Tuple, z...)::Tuple = (x..., tuplejoin(y, z...)...)
